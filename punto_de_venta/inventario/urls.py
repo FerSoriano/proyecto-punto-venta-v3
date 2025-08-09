74421f958f4f1f9fd9f03898f28d5195baa67085
FerSoriano <fer8f10@hotmail.com>
2025-08-09 15:05:42 -0600

    starting product app.

---
+++ punto_de_venta/inventario/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('productos/', views.lista_productos, name='lista_productos'),
+    path('productos/nuevo/', views.crear_producto, name='crear_producto'),
+    path('productos/<int:id>/editar/', views.editar_producto, name='editar_producto'),
+    path('productos/<int:id>/eliminar/', views.eliminar_producto, name='eliminar_producto'),
+]
