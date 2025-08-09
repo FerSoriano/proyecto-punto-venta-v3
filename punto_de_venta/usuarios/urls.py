5ef2ae7c5bf2a23d808b19836c2285588e7872fd
FerSoriano <fer8f10@hotmail.com>
2025-08-09 15:06:27 -0600

    change templates in usuarios app

--- punto_de_venta/usuarios/urls.py
+++ punto_de_venta/usuarios/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login', views.login, name='login'),
-    path('inicio/admin', views.dashboard_admin, name='dashboard_admin'),
-    path('inicio/ventas', views.dashboard_ventas, name='dashboard_ventas'),
+    path('admin/dashboard', views.dashboard_admin, name='dashboard_admin'),
+    path('ventas/dashboard', views.dashboard_ventas, name='dashboard_ventas'),
 ]
